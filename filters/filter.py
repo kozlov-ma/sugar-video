7dfccc2bc523b7c68f0a9a8bc18b8a9eee6b42ad
Mikhail Kozlov <kozlov_ma+nespamte@icloud.com>
2023-11-09 13:37:12 +0500

    Added:  - Crossfade for concat  - Rotate  - VideoTrack  - AudioTrack  - UniteAudioVideo

--- filters/filter.py
+++ filters/filter.py
@@ -143,9 +143,6 @@ class SpeedX(Filter):
 
         new_clip = in_clip.create_new()
         out_file = new_clip.source
-        if new_clip.file_exists:
-            print(f"old clip: {new_clip}")
-            return new_clip
 
         audio = ffmpeg.input(in_clip.source).audio
         video = ffmpeg.input(in_clip.source).video
@@ -163,9 +160,112 @@ class SpeedX(Filter):
 
 
 @dataclasses.dataclass(repr=True)
+class Rotate(Filter):
+    clockwise: bool
+    flip: bool
+    filter: typing.Union[Filter, None] = None
+
+    def __call__(self) -> Clip:
+        if self.filter is None:
+            return None
+
+        in_clip = self.filter()
+
+        new_clip = in_clip.create_new()
+        out_file = new_clip.source
+
+        audio = ffmpeg.input(in_clip.source).audio
+        video = ffmpeg.input(in_clip.source).video
+
+        ftype = "clock" if self.clockwise else "cclock" + "_flip" if self.flip else ""
+
+        video = video.filter("transpose", ftype)
+
+        create_dirs(out_file)
+        ffmpeg.output(video, audio, filename=out_file).overwrite_output().run()
+
+        return new_clip
+
+    def set_filter(self, filter: Filter | None = None, index=0):
+        self.filter = filter
+
+
+@dataclasses.dataclass(repr=True)
+class VideoTrack(Filter):
+    filter: typing.Union[Filter, None] = None
+
+    def __call__(self) -> Clip:
+        if self.filter is None:
+            return None
+
+        in_clip = self.filter()
+
+        new_clip = in_clip.create_new()
+        out_file = new_clip.source
+        create_dirs(out_file)
+
+        video = ffmpeg.input(in_clip.source).video
+        ffmpeg.output(video, filename=out_file).overwrite_output().run()
+
+    def set_filter(self, filter: Filter | None = None, index=0):
+        self.filter = filter
+
+
+@dataclasses.dataclass(repr=True)
+class AudioTrack(Filter):
+    filter: typing.Union[Filter, None] = None
+
+    def __call__(self) -> Clip:
+        if self.filter is None:
+            return None
+
+        in_clip = self.filter()
+
+        new_clip = in_clip.create_new()
+        out_file = new_clip.source
+        create_dirs(out_file)
+
+        audio = ffmpeg.input(in_clip.source).audio
+        ffmpeg.output(audio, filename=out_file).overwrite_output().run()
+
+    def set_filter(self, filter: Filter | None = None, index=0):
+        self.filter = filter
+
+
+@dataclasses.dataclass(repr=True)
+class UniteAudioVideo(Filter):
+    audio: Filter | None = None
+    video: Filter | None = None
+
+    def __call__(self) -> Clip:
+        if self.audio is None or self.video is None:
+            return None
+
+        in_audio = ffmpeg.input(self.audio()).audio
+        in_video = ffmpeg.input(self.video()).video
+
+        new_name = f"{in_audio.name} + {in_video.name}"
+        new_clip = Clip(new_name)
+
+        ffmpeg.output(in_video, in_audio, filename=new_clip.source).overwrite_output().run()
+
+        return new_clip
+
+    def set_filter(self, filter: Filter | None = None, index=0):
+        match index:
+            case 0:
+                self.audio = filter
+                print("set filter 0")
+            case 1:
+                self.video = filter
+                print("set filter 1")
+
+
+@dataclasses.dataclass(repr=True)
 class Concat(Filter):
     first: Filter | None = None
     second: Filter | None = None
+    smooth: bool = False
 
     def __call__(self) -> Clip:
         if self.first is None or self.second is None:
@@ -173,6 +273,9 @@ class Concat(Filter):
 
         in_first = self.first()
         in_second = self.second()
+
+        duration1 = ffmpeg.probe(in_first.source)['format']['duration']
+        duration2 = ffmpeg.probe(in_second.source)['format']['duration']
 
         audio_first = ffmpeg.input(in_first.source).audio
         video_first = ffmpeg.input(in_first.source).video
@@ -184,9 +287,19 @@ class Concat(Filter):
         new_clip = Clip(new_name)
 
         audio = ffmpeg.concat(audio_first, audio_second, a=1, v=0)
-        video = ffmpeg.concat(video_first, video_second)
+        if self.smooth:
+            fade_duration = 1
+            fade_in = f"fade=t=in:st=0:d={fade_duration}"
+            fade_out = f"fade=t=out:st={duration2 - fade_duration}:d={fade_duration}"
 
-        ffmpeg.output(video, audio, filename=new_clip.source).overwrite_output().run()
+            filter_graph = f"[0:v] {fade_out} [fv]; [1:v] {fade_in} [nextv]; [fv][nextv] overlay=eof_action=pass[outv]"
+            video = ffmpeg.concat(video_first, video_second)
+
+            ffmpeg.output(video, audio, filename=new_clip.source, filter_complex=filter_graph).overwrite_output().run()
+        else:
+            video = ffmpeg.concat(video_first, video_second)
+
+            ffmpeg.output(video, audio, filename=new_clip.source).overwrite_output().run()
 
         return new_clip
 
